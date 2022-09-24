9016f31067e158221123187fd32e76c6431e3893
25Shapes <118103388+25Shapes@users.noreply.github.com>
2022-09-24 21:27:25 +0000

    Create Structure_Abstract1.md

--- src/data.rs
+++ src/data.rs
@@ -4,3 +4,13 @@ use serde_derive::Serialize;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Model {
+    #[default]
+    GptTurbo,
+    GptTurbo0301,
+}
+
+impl Model {
+    pub(crate) fn as_str(&self) -> &'static str {
+        match self {
+            Model::GptTurbo => "gpt-3.5-turbo",
+            Model::GptTurbo0301 => "gpt-3.5-turbo-0301",
