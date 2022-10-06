d076a84093d333cee3b740ce69594812bb50ffe0
25Shapes <118103388+25Shapes@users.noreply.github.com>
2022-10-06 08:05:03 +0000

    isdn/gigaset: fix zero size border case in debug dump

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,4 @@ pub async fn async_completions(
     key: String,
     model: data::Model,
     messages: Vec<data::Message>,
-) -> Result<data::Completi
+) -> Result<data::CompletionResponse, Error> {
