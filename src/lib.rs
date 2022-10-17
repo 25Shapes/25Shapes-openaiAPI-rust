76d06428b56ef2575f1bf05f2ef7762c4d6188f6
25Shapes <118103388+25Shapes@users.noreply.github.com>
2022-10-17 11:45:09 +0000

    Added coverage

--- src/lib.rs
+++ src/lib.rs
@@ -16,4 +16,4 @@ pub async fn async_completions(
     let request_body_json = serde_json::json!(request_body).to_owned().to_string();
     let client = reqwest::Client::new();
     let res = client
-        .post("htt
+        .post("https://api.op
