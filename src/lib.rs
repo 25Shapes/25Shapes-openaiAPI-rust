3b337b4816ca931a5595ec19b5f9e7bb077cf1a2
25Shapes <118103388+25Shapes@users.noreply.github.com>
2022-11-11 06:37:01 +0000

    8.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -30,4 +30,4 @@ pub async fn async_completions(
                     Ok(response) => {
                         return Ok(response);
                     }
-                    Err(e) => return Err(Error::new(ErrorKind::Other,
+                    Err(e) => return Err(Error::new(ErrorKind::Other, e.to_string()
