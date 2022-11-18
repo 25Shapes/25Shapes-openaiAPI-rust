569e5b0e9aba3214ab68d2a17201f6f8868f29cc
25Shapes <118103388+25Shapes@users.noreply.github.com>
2022-11-18 15:00:35 +0000

    added method category to spec.json

--- src/lib.rs
+++ src/lib.rs
@@ -33,4 +33,5 @@ pub async fn async_completions(
                     Err(e) => return Err(Error::new(ErrorKind::Other, e.to_string())),
                 };
             }
-            Err(e) => Err(Error::new(ErrorKind::Other, e.to_str
+            Err(e) => Err(Error::new(ErrorKind::Other, e.to_string())),
+   
