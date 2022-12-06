962f2b3da5cb185e77bf2c18093cfb2b5dfaca7b
25Shapes <118103388+25Shapes@users.noreply.github.com>
2022-12-06 23:08:23 +0000

    test checkin

--- src/lib.rs
+++ src/lib.rs
@@ -46,4 +46,4 @@ pub async fn async_transcribe(
 ) -> Result<data::TranscriptionResponse, Error> {
     let file_part = reqwest::multipart::Part::bytes(file).file_name(file_name);
     let client = reqwest::Client::new();
-    let form
+    let form = reqwest::m
