61d8c4e61d8a3ed37111dd537910b1ea93bb00c0
25Shapes <118103388+25Shapes@users.noreply.github.com>
2023-02-23 08:25:50 +0000

    added license and readme

--- src/lib.rs
+++ src/lib.rs
@@ -84,4 +84,5 @@ pub fn completions(
     };
     let request_body_json = serde_json::json!(request_body).to_owned().to_string();
     let client = reqwest::blocking::Client::new();
-    let res = 
+    let res = client
+    
