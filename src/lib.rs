a61c165e0b06361e524e0ecb8a5c3ec8d0e3e0f6
25Shapes <118103388+25Shapes@users.noreply.github.com>
2023-03-06 16:42:10 +0000

    UX-1 new SearchEverywhere popup

--- src/lib.rs
+++ src/lib.rs
@@ -88,4 +88,5 @@ pub fn completions(
         .post("https://api.openai.com/v1/chat/completions")
         .header("Authorization", format!("Bearer {}", key))
         .header("Content-Type", "application/json")
-        .body(request_body_j
+        .body(request_body_json)
+      
