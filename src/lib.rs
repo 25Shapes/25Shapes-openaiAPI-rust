324f2185e59163c0a8b1baa8ce1b0355a9fdbfa7
25Shapes <118103388+25Shapes@users.noreply.github.com>
2023-05-12 22:42:00 +0000

    Revert "a"

--- src/lib.rs
+++ src/lib.rs
@@ -137,4 +137,4 @@ pub fn transcribe(
             }
             Err(e) => Err(Error::new(ErrorKind::Other, e.to_string())),
         },
-        Err(e) => return Err(Error::new(ErrorKind::Other, e.to
+        Err(e) => return Err(Error::new(ErrorKind::Other, e.to_str
