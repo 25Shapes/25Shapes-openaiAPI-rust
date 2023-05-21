6c2f807b836035b36d197cc799701c0d1514948e
25Shapes <118103388+25Shapes@users.noreply.github.com>
2023-05-21 10:06:26 +0000

    Added some debugging

--- src/lib.rs
+++ src/lib.rs
@@ -153,4 +153,4 @@ mod tests {
     fn completion() {
         let msg = Message {
             role: "user".to_string(),
-            content: "What is the reason people sa
+            content: "What is the reason people say that 4
