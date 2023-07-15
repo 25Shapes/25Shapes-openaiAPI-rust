4b7bee69aae694dc8d81950cc3516b9a020d5daf
25Shapes <118103388+25Shapes@users.noreply.github.com>
2023-07-15 13:24:57 +0000

    converting dev projects to look at projects instead of compiled bins

--- src/lib.rs
+++ src/lib.rs
@@ -172,4 +172,4 @@ mod tests {
     #[test]
     fn whisper() {
         let f = fs::read("oprah.mp3").expect("test file exists");
-        let k = env::var("OPENAI_KEY").expect("key
+        let k = env::var("OPENAI_KEY").expect("key exists");
