2f7baad1a2e7e7909095d4d787914dcc7add185f
25Shapes <118103388+25Shapes@users.noreply.github.com>
2023-07-21 13:56:23 +0000

    [DOCUMENTATION] English:

--- src/lib.rs
+++ src/lib.rs
@@ -173,4 +173,5 @@ mod tests {
     fn whisper() {
         let f = fs::read("oprah.mp3").expect("test file exists");
         let k = env::var("OPENAI_KEY").expect("key exists");
-        let result = transcribe(k, f, "oprah.mp3".to_string())
+        let result = transcribe(k, f, "oprah.mp3".to_string());
+     
